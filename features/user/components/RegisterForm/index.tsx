476732b1c10ba336781259e3912c01d5739e2f84
Gonçalo Marantes <goncalojmarantes@gmail.com>
2025-03-10 19:14:35 +0000

    chore(RegisterForm): add log

--- features/user/components/RegisterForm/index.tsx
+++ features/user/components/RegisterForm/index.tsx
@@ -7,34 +7,42 @@ const RegisterForm: React.FC = () => {
   const form = useForm({
     values: {
       phone: "",
-    }
+    },
   })
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
 
   return (
     <Form {...form}>
-      <FormField
-        control={form.control}
-        name="phone"
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>
-              Phone Number
-            </FormLabel>
-            <FormControl>
-              <Input
-                type="tel"
-                placeholder='Phone Number'
-                {...field}
-              />
-            </FormControl>
-            <FormDescription>
-              We'll send you a text with a confirmation code
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-      <Button type="submit" className='w-full'>Register</Button>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Phone Number
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type="tel"
+                  placeholder='Phone Number'
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                We'll send you a text with a confirmation code
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button disabled={!form.formState.isValid} type="submit" className="w-full">
+          Register
+        </Button>
+      </form>
     </Form>
   )
 }
