56398777e0f9b067febb202f2c4a07e284b84da0
Gonçalo Marantes <goncalojmarantes@gmail.com>
2025-03-20 18:51:46 +0000

    chore: add user stripe customer id migration

--- src/database/migrations/index.ts
+++ src/database/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250319_132743_init from './20250319_132743_init';
+import * as migration_20250320_185007_user_stripe_customer_id from './20250320_185007_user_stripe_customer_id';
 
 export const migrations = [
   {
     up: migration_20250319_132743_init.up,
     down: migration_20250319_132743_init.down,
-    name: '20250319_132743_init'
+    name: '20250319_132743_init',
+  },
+  {
+    up: migration_20250320_185007_user_stripe_customer_id.up,
+    down: migration_20250320_185007_user_stripe_customer_id.down,
+    name: '20250320_185007_user_stripe_customer_id'
   },
 ];
