f8f7b39937e22a1812b822d8411f58d094de2c95
Gonçalo Marantes <goncalojmarantes@gmail.com>
2025-03-31 20:25:39 +0100

    feature: start basic invite page

--- src/domains/games/get-game-by-id/get-game-by-id.service.ts
+++ src/domains/games/get-game-by-id/get-game-by-id.service.ts
@@ -1,14 +1,15 @@
 import { Game } from '@payload-types'
-import { getPayload } from 'payload'
+import { getPayload, TypedLocale } from 'payload'
 import config from '@payload-config'
 import { GameModel } from '../shared/models/Game.model'
 
-export async function getGameById(id: Game['id']) {
+export async function getGameById(id: Game['id'], locale?: TypedLocale) {
   const payload = await getPayload({ config })
 
   const game = await payload.findByID({
     collection: 'games',
     id,
+    locale,
   })
 
   return GameModel.from(game)
