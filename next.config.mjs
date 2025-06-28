5453ad31dc0f61a0dfc1d5a527945af8b594454c
Louis Monteiro <louismonteiro@gmail.com>
2025-06-28 15:46:53 +0100

    remove APP_URL var

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,6 @@ const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_APP_URL: `https://${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_APP_URL}`,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
